ba62f03e86682f6fba03de76e333492c01f5711a
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-04-19 22:24:29 +0200

    Layout: move some theme assets to public/

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -11,10 +11,10 @@ function scrollToNews(e) {
 <template>
   <div class="relative">
     <!-- on small screens: background image is always there -->
-    <div class="sm:block absolute inset-0 bg-cover bg-center" style="background-image: url('/assets/2026-04-19-getrudes-konzerts-flat.png'); filter: saturate(0.5) brightness(1.0)" v-if="frontmatter.isHome"></div>
+    <div class="sm:block absolute inset-0 bg-cover bg-center" style="background-image: url('/2026-04-19-getrudes-konzerts-flat.png'); filter: saturate(0.5) brightness(1.0)" v-if="frontmatter.isHome"></div>
     <div class="sm:block absolute inset-0 bg-cover bg-center" style="background: #191415;" v-else></div>
 
-    <div class="block sm:hidden absolute inset-0 bg-cover bg-center" style="background-image: url('/assets/2026-04-19-getrudes-konzerts-flat.png'); filter: saturate(0.5) brightness(1.0)"></div>
+    <div class="block sm:hidden absolute inset-0 bg-cover bg-center" style="background-image: url('/2026-04-19-getrudes-konzerts-flat.png'); filter: saturate(0.5) brightness(1.0)"></div>
 
     <!-- on small screens: hero appears before menu and is always there -->
 
@@ -23,12 +23,12 @@ function scrollToNews(e) {
       <nav class="block flex items-center justify-between px-[1em] pt-1 max-w-[1000px] m-auto text-center uppercase text-[#eee] font-semibold">
         <div class="block flex items-center justify-between w-full pt-3">
           <a href="/" @click.prevent.stop="scrollToNews" class="block hover:text-white font-medium order-0 text-xl p-2">News</a>
-          <a href="/" class="block inline mb-5 mr-auto ml-auto"><img src="/assets/logo-white.png" alt="logo" class="h-[70px]"></a>
+          <a href="/" class="block inline mb-5 mr-auto ml-auto"><img src="/logo-white.png" alt="logo" class="h-[70px]"></a>
           <a href="/contact" class="block hover:text-white font-medium order-2 text-xl p-2">Contact us</a>
         </div>
       </nav>
       <div v-if="frontmatter.isHome" class="flex flex-col items-center gap-6 pb-[3em]">
-        <img src="/assets/logo-hero.png" alt="logo" class="w-full max-w-[800px] p-5">
+        <img src="/logo-hero.png" alt="logo" class="w-full max-w-[800px] p-5">
         <div class="flex gap-6">
           <a href="https://fb.me/AOBSharp" target="_blank"><img src="./facebook.svg" alt="Facebook" class="w-8 h-8 hover:opacity-60" style="filter: brightness(0) invert(1)"></a>
           <a href="https://www.instagram.com/AOBSharp" target="_blank"><img src="./instagram.svg" alt="Instagram" class="w-8 h-8 hover:opacity-60" style="filter: brightness(0) invert(1)"></a>
@@ -39,9 +39,9 @@ function scrollToNews(e) {
 
     <!-- READER, BEWARE: this is copypasted and lightly edited for the different formats -->
     <div class="relative sm:hidden">
-      <div class="flex items-center"><a href="/" class="block inline mb-5 mr-auto ml-auto"><img src="/assets/logo-white.png" alt="logo" class="h-[70px] p-3"></a></div>
+      <div class="flex items-center"><a href="/" class="block inline mb-5 mr-auto ml-auto"><img src="/logo-white.png" alt="logo" class="h-[70px] p-3"></a></div>
       <div class="flex flex-col items-center gap-6 pb-2">
-        <a href="/" class="block inline mb-5 mr-auto ml-auto"><img src="/assets/logo-hero.png" alt="logo" class="p-3"></a>
+        <a href="/" class="block inline mb-5 mr-auto ml-auto"><img src="/logo-hero.png" alt="logo" class="p-3"></a>
       </div>
       <nav class="block md:flex items-center justify-between px-[1em] py-1 max-w-[1000px] m-auto text-center uppercase text-[#eee] font-semibold">
         <div class="block md:flex items-center justify-between w-full pt-2 pb-4">
